a8202f64f627a94c6f13fb1aaf74ad0e974f2de3
nicobuiatti <nbuiatti@santandertecnologia.com.ar>
2021-10-28 16:54:00 -0300

    ahora si lo guardé

--- Marco1.js
+++ Marco1.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 console.log("hola marco");
 
 function paraMarco() {
     console.log("AHORA TO CAMBIO TODO EL ARCHIVO CRACK");
 }
-=======
-console.log("hola marco")
+console.log("hola marco");
 
 let nicoChusma = "deja mi archivo loco";
->>>>>>> c7259696d74a3fd5a19239b74758a6027fcd2855
